ad29c12872bfe9408fbbe6a8c22820d61eae5c9b
Piyush Sharma <piyush@lumeneo.ai>
2024-12-21 12:46:15 +0530

    rout fixes

--- app/layout.jsx
+++ app/layout.jsx
@@ -34,11 +34,11 @@ const RootLayout = ({ children }) => {
       module: [
         {
           name: "DotVision",
-          link: "/Product/Dotvision",
+          link: "/product/dotvision",
         },
         {
           name: "Dot-AI/VI",
-          link: "/Product/Dotaivi",
+          link: "/product/dotaivi",
         },
       ],
     },
